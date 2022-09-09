c05d1578c711ba9e8674b7215c8ccadc1a498fb2
Parsec <sailcali@gmail.com>
2022-09-09 13:17:55 -0700

    Fixed record loop logic

--- record.py
+++ record.py
@@ -100,7 +100,7 @@ def record(stop_time):
 	rolling_avg = RollingAverage()
 
 	# loop over the frames of the stream until the stop time is reached
-	while not stop_time.hour == datetime.now().hour and stop_time.minute == datetime.now().minute:
+	while stop_time.hour != datetime.now().hour or stop_time.minute != datetime.now().minute:
 		frame = get_frame(vs)
 		
 		# if the frame could not be grabbed, then we have reached the end
